6a5d96dc0502ff154ea7ae5020a3eef323cd95ab
mazp-sogeti <marceluszp@gmail.com>
2017-05-05 12:46:13 +0200

    third commit

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,6 +2,8 @@ export class Recipe {
   public name:string;
   public description: string;
   public imagePath:string;
+ public imagePath2:string;
+  
   constructor(name:string, desc:string, imagePath:string){
     this.name=name;
     this.description=desc;
